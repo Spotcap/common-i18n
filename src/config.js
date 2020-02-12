b0bd33ac749f2993364cf3013393aa6da3215de0
mounikakonduru117 <mounika.konduru@external.spotcap.com>
2020-02-12 21:32:59 +0530

    Removed accesskey and secretkey

--- src/config.js
+++ src/config.js
@@ -12,9 +12,6 @@ module.exports = class Config {
     this._CACHE_MINUTES = _.get(options, "CACHE_MINUTES", 0);
     this._WORKSPACE = _.get(options, "WORKSPACE", null);
     this._BUCKET = _.get(options, "S3_BUCKET", null);
-    this._ACCESS_KEY_ID = _.get(options, "S3_ACCESS_KEY_ID", null);
-    this._SECRET_ACCESS_KEY = _.get(options, "S3_SECRET_ACCESS_KEY", null);
-    this._REGION = _.get(options, "S3_REGION", null);
     this._LOKALISE_BASE_URL = _.get(options,"LOKALISE_BASE_URL",null);
     this._CEMBRA_PROJECT_ID = _.get(options,"CEMBRA_PROJECT_ID",null);
     this._LOKALISE_API_TOKEN = _.get(options,"LOKALISE_API_TOKEN",null);
@@ -30,8 +27,6 @@ module.exports = class Config {
       _CEMBRA_PROJECT_ID : this._CEMBRA_PROJECT_ID,
       _LOKALISE_API_TOKEN : this._LOKALISE_API_TOKEN,
       _S3: new aws.S3({
-        accessKeyId: this._ACCESS_KEY_ID,
-        secretAccessKey: this._SECRET_ACCESS_KEY,
         region: this._REGION,
         params: {
           Bucket: this._BUCKET
